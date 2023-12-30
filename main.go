eff1844b590d3f5cd269658a31bd9c987d22fa4e
balazs4 <balazs4@users.noreply.github.com>
2023-12-30 23:50:55 +0100

    yolo CoMmIt :sponge:

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		index("World").Render(context.Background(), w)
+		fmt.Printf("[%s] %s\n", r.Method, r.URL)
+		switch r.Method {
+		case "GET":
+			index().Render(context.Background(), w)
+		case "POST":
+			r.ParseForm()
+			fmt.Println(r.PostForm)
+		default:
+
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
