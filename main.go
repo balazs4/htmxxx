a93b739d74a6f4a7b46d0969ac290ab3127c4a09
balazs4 <balazs4@users.noreply.github.com>
2024-01-01 23:57:54 +0100

    dx improvement
    radio bob metalcore ist geil

--- main.go
+++ main.go
@@ -5,21 +5,48 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	if os.Getenv("WATCH") == "1" {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGTERM)
+
+		http.HandleFunc("/.sigterm", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("content-type", "text/event-stream")
+			w.Header().Add("connection", "keep-alive")
+			w.Header().Add("cache-control", "no-cache")
+
+			<-sigs
+			sigterm := fmt.Sprintf("data: SIGTERM pid %d \n\n", os.Getpid())
+			w.Write([]byte(sigterm))
+		})
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("[%s] %s\n", r.Method, r.URL)
+
 		switch r.Method {
-		case "GET":
+
+		case http.MethodGet:
 			index().Render(context.Background(), w)
-		case "POST":
+
+		case http.MethodPost:
 			r.ParseForm()
 			fmt.Println(r.PostForm)
-		default:
 
 		}
+	})
+
+	http.HandleFunc("/htmx.min.js", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "htmx.min.js")
+	})
+
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
 	})
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
--- makefile
+++ makefile
@@ -1,17 +1,17 @@
 default:
 	@test `which templ`     || echo 'go install github.com/a-h/templ/cmd/templ@latest'
 	@test `which watchexec` || echo 'https://github.com/watchexec/watchexec?tab=readme-ov-file#install'
-dev:
-	@watchexec --print-events -c -r --project-origin . -- make --silent --always-make run
+watch:
+	@WATCH=1 watchexec --print-events --no-meta -c -r --project-origin . --stop-timeout 0 -- make --always-make --silent run
 
 fmt:
 	@go fmt
 
 run:
-	@templ generate
-	@go run .
+	@templ generate && go run .
 
-build:
-	@templ generate
-	@go build
+htmxxx:
+	@templ generate && go build .
 
+htmx.min.js:
+	curl https://unpkg.com/htmx.org@1.9.10/dist/htmx.min.js -O
